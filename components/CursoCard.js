abe55915110ce2eeec55d5668873f65515a9b726
Juan David <gottygarcia@gmail.com>
2020-06-12 02:18:23 -0500

    avances en panel de cursos

---
+++ components/CursoCard.js
@@ -0,0 +1,5 @@
+const CursoCard = () => {
+	return <div className="curso-card"></div>
+}
+
+export default CursoCard
--- pages/index.js
+++ pages/index.js
@@ -1,203 +1,91 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import CursoCard from '../components/CursoCard'
+import '../css/app.css'
 
 const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
+	<>
+		<Head>
+			<title>Plataforma educativa</title>
+			<link rel="icon" href="/favicon.ico" />
+			<link
+				href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+				rel="stylesheet"
+			></link>
+		</Head>
+		<div className="container-app">
+			<header className="header-app">
+				<div className="search-header">
+					<img src="/img/icons/search.svg" alt="" />
+					<input placeholder="Buscar curso" type="text" />
+					<img src="" alt="" />
+				</div>
+				<div className="btns-header">
+					<button>
+						<img src="/img/icons/bell.svg" alt="Notifications" />
+						<div className="circle-nofification"></div>
+					</button>
+					<button>
+						<img src="/img/icons/task.svg" alt="Task" />
+					</button>
+					<button>Nuevo curso</button>
+				</div>
+			</header>
+			<nav className="nav-app">
+				<Link href="/">
+					<a className="logo">
+						<img src="/img/logo.png" alt="Logo" />
+					</a>
+				</Link>
+				<ul>
+					<li>
+						<Link href="/">
+							<a>
+								Cursos
+								<img src="/img/icons/book.svg" alt="" />
+							</a>
+						</Link>
+					</li>
+					<li>
+						<Link href="/">
+							<a>
+								Calendario
+								<img src="/img/icons/calendar.svg" alt="" />
+							</a>
+						</Link>
+					</li>
+					<li>
+						<Link href="/">
+							<a>
+								Tareas pendientes
+								<img src="/img/icons/task.svg" alt="" />
+							</a>
+						</Link>
+					</li>
+				</ul>
+				<div className="promo-nav">
+					<img src="/img/illustrations/mano.png" alt="Promo" />
+					<p>La cuenta Pro es más poderosa. obtenga un 30% de descuento.</p>
+					<button>Obtener Pro</button>
+				</div>
+				<div className="profile-nav">
+					<img src="/img/app/profile.jpg" alt="Profile" />
+					<div>
+						<span>Lana García</span>
+						<span>Estudiante</span>
+					</div>
+				</div>
+			</nav>
+			<main className="main-app">
+				<h1>Cursos</h1>
+				<div className="contenedor-cursos">
+					<CursoCard />
+					<CursoCard />
+					<CursoCard />
+				</div>
+			</main>
+		</div>
+	</>
 )
 
 export default Home
